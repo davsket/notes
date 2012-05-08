e22cebf481fac719e5319e0224a19dbc267f4bde
David Avellaneda <g.avella@gmail.com>
2012-05-07 20:09:12 -0500

    adding the email and password pulling from the server

--- web.js
+++ web.js
@@ -1,43 +1,48 @@
 var express = require('express'),
 	app = express.createServer(express.logger()),
 	email = require('mailer'),
-	redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+	redis = require('redis-url').connect(process.env.REDISTOGO_URL),
+	myEmail = 'foo@foo.com',
+	myPassword = 'le_password';
 
 app.use(express.static(__dirname+'/'));
+
+//Getting the application's email and password 
+redis.get('notesapp_email', function(err, value) {
+	myEmail = value;
+});
+redis.get('notesapp_email_password', function(err, value) {
+	myPassword = value;
+});
 
 app.get('/test', function(request, response) {
 	var req = require('url').parse(request.url, true),
 		shortNote = req.query.note.replace(/<\/?[^<>]+>/g,'').substring(0,15);
-	
-
-	redis.set('foo', 'bar');
-
-	redis.get('foo', function(err, value) {
-		console.log('foo is: ' + value);
-	});
 
 	shortNote = shortNote.length > 15 ? shortNote + '...' : shortNote;
 
-	// email.send(
-	// 	{
-	// 		host : "smtp.gmail.com",            // smtp server hostname
-	// 		port : "25",                     	// smtp server port
-	// 		domain : "davset.me",            	// domain used by client to identify itself to server
-	// 		to : req.query.email,
-	// 		from : "no-reply@davsket.me",
-	// 		subject : "Shared note: " + shortNote,
-	// 		template : "email.html.txt",   // path to template name
-	// 	    data : {
-	// 	      "text": req.query.note
-	// 	    },
-	// 		authentication : "login",        	// auth login is supported; anything else is no auth
-	// 	},
-	// 	function(err, result){
-	// 		if(err){ 
-	// 			console.log(err); 
-	// 		}
-	// 	}
-	// );
+	email.send(
+		{
+			host : "smtp.gmail.com",            // smtp server hostname
+			port : "25",                     	// smtp server port
+			domain : "davset.me",            	// domain used by client to identify itself to server
+			to : req.query.email,
+			from : "no-reply@davsket.me",
+			subject : "Shared note: " + shortNote,
+			template : "email.html.txt",   		// path to template name
+			username: myEmail,
+			password: myPassword,
+		    data : {
+		      "text": req.query.note
+		    },
+			authentication : "login",        	// auth login is supported; anything else is no auth
+		},
+		function(err, result){
+			if(err){ 
+				console.log(err); 
+			}
+		}
+	);
 
   	response.send(req.query.note);
 });
