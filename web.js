d99494159db9501233e6e327a2e31c5be9892ef4
David Avellaneda <g.avella@gmail.com>
2012-05-27 22:08:13 -0500

    adding the moving on message

--- web.js
+++ web.js
@@ -1,72 +1,49 @@
 var express = require('express'),
-	app = express.createServer(express.logger()),
-	email = require('mailer'),
-	redis = require('redis-url').connect(process.env.REDISTOGO_URL),
-	myEmail = 'foo@foo.com',
-	myPassword = 'le_password';
+	routes = require('./routes'),
+	app = express.createServer(express.logger());
 
-console.log(redis.get('hola',function(e,v){console.log(v+'++++++++++++++');}))
-app.use(express.bodyParser());
+// console.log(redis.get('hola',function(e,v){console.log(v+'++++++++++++++');}))
 
-//Getting the application's email and password 
-redis.get('notesapp_email', function(err, value) {
-	myEmail = value;
-});
-redis.get('notesapp_email_password', function(err, value) {
-	myPassword = value;
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/static'));
 });
 
-app.post('/load/', function(request, response) {
-	console.log('----------------------');
-	var shortNote = '';
-	console.log(request.body.notes);
-	response.redirect('/');
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.get('/load/', function(request, response) {
-	console.log('----------------------g');
-	var shortNote = '';
-	console.log(request.body.notes);
-	response.redirect('/');
+app.configure('production', function(){
+  app.use(express.errorHandler());
 });
 
-app.post('/test', function(request, response) {
-	var shortNote = '';
-	console.log(request);
-	if(request.body.note && request.body.email){
-		shortNote = request.body.note.replace(/<\/?[^<>]+>/g,'').substring(0,15);
-		shortNote = shortNote.length > 15 ? shortNote + '...' : shortNote;
+app.get('/save/access_token', routes.save_token);
 
-		email.send(
-			{
-				host : "smtp.gmail.com",            // smtp server hostname
-				port : "25",                     	// smtp server port
-				domain : "davset.me",            	// domain used by client to identify itself to server
-				to : request.body.email,
-				from : "no-reply@davsket.me",
-				subject : "Shared note: " + shortNote,
-				template : "email.html.txt",   		// path to template name
-				username: myEmail,
-				password: myPassword,
-			    data : {
-			      "text": request.body.note
-			    },
-				authentication : "login",        	// auth login is supported; anything else is no auth
-			},
-			function(err, result){
-				if(err){ 
-					console.log(err); 
-				}
-			}
-		);
+app.all('/', routes.index);
+app.all('/load', routes.load);
+app.post('/test', routes.email);
 
-	  	response.send('{"status": true, "message": "ok, email sent"}');	
-	}else{
-		response.send('{"status": false, "message": "not enough params"}');
-	}
-});
 
-app.use('/', express.static(__dirname + '/static'));
+
+
+// app.post('/load/', function(request, response) {
+// 	console.log('----------------------');
+// 	var shortNote = '';
+// 	console.log(request.body.notes);
+// 	response.redirect('/');
+// });
+
+// app.get('/load/', function(request, response) {
+// 	console.log('----------------------g');
+// 	var shortNote = '';
+// 	console.log(request.body.notes);
+// 	response.redirect('/');
+// });
+
 // app.use(express.static(__dirname+'/static'));
 
 var port = process.env.PORT || 3000;
